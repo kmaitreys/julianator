39d5614c23a338381aba3677902bb796ec9685d5
kmaitreys <peakcipher1729@gmail.com>
2023-10-11 06:49:26 +0530

    refactored into functions

--- advent-of-code/day-1/day-1.jl
+++ advent-of-code/day-1/day-1.jl
@@ -1,34 +1,45 @@
-input = open("/home/ws2/Documents/stuff/julianator/advent-of-code/input.txt", "r")
+input = open("/home/ws2/Documents/stuff/julianator/advent-of-code/day-1/day-1.txt", "r")
 input_data = readlines(input)
 close(input)
 
-int_array = zeros(Int32, length(input_data))
 
-for i in eachindex(input_data)
-    if isempty(input_data[i])
-        continue
-    else
-        int_array[i] = parse(Float64, input_data[i])
+function calculate_total_calories(input_data)
+    int_array = zeros(Int32, length(input_data))
+    for i in eachindex(input_data)
+        if isempty(input_data[i])
+            continue
+        else
+            int_array[i] = parse(Int32, input_data[i])
+        end
     end
+    tot_calories = Int32[]
+    tmp = 0
+    for i in eachindex(int_array)
+        if int_array[i] != 0
+            tmp = tmp + int_array[i]
+        else
+            push!(tot_calories, tmp)
+            tmp = 0
+        end
+    end
+    return tot_calories
 end
 
-tot_calories = Int32[]
-global tmp = 0
-
-for i in eachindex(int_array)
-    if int_array[i] != 0
-        tmp = tmp + int_array[i]
-    else
-        push!(tot_calories, tmp)
-        global tmp = 0
-    end
+function get_max_calories(total_calories)
+    return maximum(total_calories)
 end
 
-println(maximum(tot_calories))
+function get_top_three_sum(total_calories)
+    sort!(total_calories, rev=true)
+    top_three_sum = sum(total_calories[1:3])
+    return top_three_sum    
+end
 
-sort!(tot_calories, rev=true)
+part_1 = get_max_calories(calculate_total_calories(input_data))
+part_2 = get_top_three_sum(calculate_total_calories(input_data))
 
-print(sum(tot_calories[1:3]))
+println("The answer to first part is $part_1.")
+println("The answer to second part is $part_2.")
 
 
     
